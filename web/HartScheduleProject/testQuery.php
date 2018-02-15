5a8dfe071b9437db5219cbee3980663b29e7f4ca
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-15 14:36:30 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -87,16 +87,18 @@ tr:nth-child(even) {
             {
               echo '<th>' . $dutyrow['dutyname'] . '</th>';
 
-                //get eployees
-                echo '<tr>';
+
                 foreach ($db->query('SELECT e.firstname, sh.shiftname, d.dutyname FROM employee e
                     JOIN schedule s on e.employeeid = s.scheduleid
                     JOIN shift sh on sh.shiftid = s.scheduleid
                     JOIN duty d on d.dutyid = s.scheduleid') as $row)
                 {
-                    echo '<td>' . $row['firstname'] . '</td>';
+                  //get eployees
+                  echo '<tr>';
+                  echo '<td>' . $row['firstname'] . '</td>';
+                  echo '</tr>';
                 }
-                echo '</tr>';
+
 
             }
       //end massive row
