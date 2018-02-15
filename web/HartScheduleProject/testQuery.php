308997b69df383e3fa8cc1c962f0dd7f92ab80ef
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-15 16:48:58 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -83,7 +83,7 @@ tr:nth-child(even) {
       //start massive row...
       echo '<tr>';
           //get shifts
-          echo '<td colspan="3" rowspan="6">' . $shiftrow['shiftname'] . '</td>';
+          echo '<td rowspan="6">' . $shiftrow['shiftname'] . '</td>';
           $shiftid = $shiftrow['shiftid'];
             //get duties...
             // echo '<tr>';
@@ -96,9 +96,9 @@ tr:nth-child(even) {
               //for each duty, get all the employee's who have been assigned to it...
                 foreach ($db->query('SELECT firstname FROM employee e') as $row)
                 {
-                  echo '<tr>';
+                //  echo '<tr>';
                   echo '<td>' . $row['firstname'] . '</td>';
-                  echo '</tr>';
+                  //echo '</tr>';
                 }
               }
         //end massive row
