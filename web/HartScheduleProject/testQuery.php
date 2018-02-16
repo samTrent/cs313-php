84e98006299c6886a065eccf07747d2a0e0d0843
sd.trent@yahoo.com <sd.trent@yahoo.com>
2018-02-15 20:16:15 -0700

    testing

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -129,7 +129,7 @@ tr:nth-child(even) {
       //start massive row...
       echo '<tr>';
           //get shifts
-          echo '<td>' . $shiftrow['shiftname'] . '</td>';
+          echo '<td rowspan="2">' . $shiftrow['shiftname'] . '</td>';
           $shiftid = $shiftrow['shiftid'];
             //get duties...
 
