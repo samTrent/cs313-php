7e71fdcbc5ad618c494214feb51f16a8c654ccaa
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-15 18:51:08 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -83,7 +83,7 @@ tr:nth-child(even) {
       //start massive row...
       echo '<tr>';
           //get shifts
-          echo '<td rowspan="5">' . $shiftrow['shiftname'] . '</td>';
+          echo '<td rowspan="6">' . $shiftrow['shiftname'] . '</td>';
           $shiftid = $shiftrow['shiftid'];
             //get duties...
 
