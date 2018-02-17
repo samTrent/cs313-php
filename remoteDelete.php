e82fd3c7681bcfe9ded5739f55ce5bf662cd27cc
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 13:11:51 -0700

    next update

---
+++ remoteDelete.php
@@ -0,0 +1,29 @@
+<?php
+
+try {
+  $host = "ec2-107-21-236-219.compute-1.amazonaws.com";
+  $dbname = "dcupbm4rvpsqb2";
+  $user = "zaqfmlhepcfhlm";
+  $password = "c9c64abec3d5de71334d351d883347b9e63e6a8b3a2d7fab5bbd3551f20112f4";
+  $port = "5432";
+
+  $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+//Clear out old data...
+$deletestmt = $db ->prepare('DELETE FROM submittedschedule WHERE submitteddate = :submitteddate');
+$deletestmt->bindValue(":submitteddate", $_POST['date'], PDO::PARAM_STR);
+if ($deletestmt->execute())
+{
+  echo "SUECCES DELETING DATA!<br>";
+}
+else
+{
+  echo "DELETING FAILED!<br>";
+}
+
+}
+catch (PDOException $ex)
+{
+ print "<p>error: $ex </p>\n\n";
+ die();
+}
+ ?>
