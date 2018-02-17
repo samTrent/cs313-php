85788ce65875c2e04f36e5bb9c8e916865df6ee3
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 18:43:09 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -127,29 +127,38 @@ tr:nth-child(even) {
     echo '</table>';
 
     echo '<p>FC EMPS</p>';
-    foreach ($db->query('SELECT e.firstname, d.duty FROM employee e
+    foreach ($db->query('SELECT e.firstname, d.duty, s.shift FROM employee e
     JOIN submittedschedule su ON e.employeeid = su.employee
-    JOIN duty d ON d.dutyid = su.duty WHERE d.duty = \'Fitness Center\'') as $row)
+    JOIN duty d ON d.dutyid = su.duty
+    JOIN shift s ON s.shiftid = su.shift
+    WHERE d.duty = \'Fitness Center\' AND s.shift = \'2PM-7PM\'') as $row)
     {
       //fitness center
+
       echo '<p>' . $row['firstname'] . '</p>'; // FC
     }
 
     echo '<p>IC EMPS</p>';
-    foreach ($db->query('SELECT e.firstname, d.duty FROM employee e
+    foreach ($db->query('SELECT e.firstname, d.duty, s.shift FROM employee e
     JOIN submittedschedule su ON e.employeeid = su.employee
-    JOIN duty d ON d.dutyid = su.duty WHERE d.duty = \'ICenter\'') as $row)
+    JOIN duty d ON d.dutyid = su.duty
+    JOIN shift s ON s.shiftid = su.shift
+    WHERE d.duty = \'ICenter\' AND s.shift = \'2PM-7PM\'') as $row)
     {
       //fitness center
+
       echo '<p>' . $row['firstname'] . '</p>'; // FC
     }
 
     echo '<p>ER EMPS</p>';
-    foreach ($db->query('SELECT e.firstname, d.duty FROM employee e
+    foreach ($db->query('SELECT e.firstname, d.duty, s.shift FROM employee e
     JOIN submittedschedule su ON e.employeeid = su.employee
-    JOIN duty d ON d.dutyid = su.duty WHERE d.duty = \'Equipment Room\'') as $row)
+    JOIN duty d ON d.dutyid = su.duty
+    JOIN shift s ON s.shiftid = su.shift
+    WHERE d.duty = \'Fitness Center\' AND s.shift = \'2PM-7PM\'') as $row)
     {
       //fitness center
+
       echo '<p>' . $row['firstname'] . '</p>'; // FC
     }
 
