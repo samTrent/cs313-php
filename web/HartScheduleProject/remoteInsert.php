7bf0b36360a3d8d15ba364e89b2df332959a49a0
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 11:07:31 -0700

    next update

--- web/HartScheduleProject/remoteInsert.php
+++ web/HartScheduleProject/remoteInsert.php
@@ -14,26 +14,26 @@ try {
   $stmt->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
   $stmt->execute();
   $employeeID = $stmt->fetch(PDO::FETCH_NUM);
-  echo 'The employeeID for '. $_POST['firstname'] . ' is :: ' . $employeeID[0] . '<br>';
+  echo 'The employeeID for '. $_POST['firstname'] . ' is :: ' . $employeeID[0] . ' <br>';
 
   //get dutyid
   $stmt = $db ->prepare("SELECT dutyid FROM duty WHERE duty = :duty");
   $stmt->bindValue(':duty', $_POST['duty'], PDO::PARAM_STR);
   $stmt->execute();
   $dutyID = $stmt->fetch(PDO::FETCH_NUM);
-  echo 'The dutyID for '. $_POST['duty'] . ' is :: ' . $dutyID[0] . '<br>';
+  echo 'The dutyID for '. $_POST['duty'] . ' is :: ' . $dutyID[0] . ' <br>';
 
-  echo 'The date is: ' . $_POST['date'] . '<br>';
-  echo 'The shift is: ' . $_POST['shift'] . '<br>';
+  echo 'The date is: ' . $_POST['date'] . ' <br>';
+  echo 'The shift is: ' . $_POST['shift'] . ' <br>';
 
-  $stmt = $db ->prepare("INSERT INTO submittedschedule (date, employee, shift, duty) VALUES (:thedate, :firstname, :shift, :duty)");
+  $finalstmt = $db ->prepare("INSERT INTO submittedschedule (date, employee, shift, duty) VALUES (:thedate, :firstname, :shift, :duty)");
 
-  $stmt->bindValue(':thedate', $_POST['date'], PDO::PARAM_STR);
-  $stmt->bindValue(':firstname', $employeeID, PDO::PARAM_INT);
-  $stmt->bindValue(':shift', $_POST['shift'], PDO::PARAM_INT);
-  $stmt->bindValue(':duty', $dutyID, PDO::PARAM_INT);
-
-  $stmt->execute();
+  $finalstmt->bindValue(':thedate', $_POST['date'], PDO::PARAM_STR);
+  $finalstmt->bindValue(':firstname', $employeeID, PDO::PARAM_INT);
+  $finalstmt->bindValue(':shift', $_POST['shift'], PDO::PARAM_INT);
+  $finalstmt->bindValue(':duty', $dutyID, PDO::PARAM_INT);
+  echo 'submitting schedule....';
+  $finalstmt->execute();
 
   // $stmt = $db ->prepare("INSERT INTO employee (firstname, lastname) VALUES (:firstname, :lastname)");
 
