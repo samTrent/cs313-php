539f36c12cb5eb2da2405d87bb30ed80949c8ad1
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 14:49:05 -0700

    next update

--- web/HartScheduleProject/remoteDelete.php
+++ web/HartScheduleProject/remoteDelete.php
@@ -10,13 +10,15 @@ try {
   $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
 
 
-echo "<p>THIS IS DATE " . $_POST['date'] . " </p>" ;
-echo "<p>THIS IS SHIFT " . $_POST['shift'] . " </p>" ;
+echo "<p>THIS IS DATE " . $_POST['date'] . " </p>";
+echo "<p>THIS IS SHIFT " . $_POST['shift'] . " </p>";
 
 //Clear out old data...
-$deletestmt = $db ->prepare('DELETE FROM submittedschedule WHERE submitteddate = :submitteddate');
-$deletestmt->bindValue(":submitteddate", $_POST['date'], PDO::PARAM_STR);
+$deletestmt = $db ->prepare('DELETE FROM submittedschedule WHERE submitteddate = :submitteddateString AND shift = :shiftid');
+
+$deletestmt->bindValue(":submitteddateString", $_POST['date'], PDO::PARAM_STR);
 $deletestmt->bindValue(":shiftid", $_POST['shift'], PDO::PARAM_INT);
+
 if ($deletestmt->execute())
 {
   echo "SUECCES DELETING DATA! AYA<br>";
