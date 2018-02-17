e7fc64799a9bb955d55291d878c25d75710047ba
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 20:46:27 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -48,7 +48,7 @@ tr:nth-child(even) {
         JOIN submittedschedule su ON e.employeeid = su.employee
         JOIN duty d ON d.dutyid = su.duty
         JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'Fitness Center\' AND s.shiftid = '. $shiftid .'') as $row)
+        WHERE d.duty = \'Fitness Center\' AND s.shiftid = '. $shiftid .' AND su.date = \''. $datestamp .'\'') as $row)
         {
           //fitness center
           array_push($FCempArray, $row['firstname']);
