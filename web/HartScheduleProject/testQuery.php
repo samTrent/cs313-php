9b7e466db72cc35fe663cad441e4d9a993dceff2
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 20:30:40 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -44,12 +44,12 @@ tr:nth-child(even) {
 
       function getFitnessCenterEmps($db, $shiftid, &$FCempArray, $datestamp)
       {
-
+        echo 'date is' . $datestamp;
         foreach ($db->query('SELECT e.firstname, d.duty, s.shift FROM employee e
         JOIN submittedschedule su ON e.employeeid = su.employee
         JOIN duty d ON d.dutyid = su.duty
         JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'Fitness Center\' AND s.shiftid = '. $shiftid .' AND su.date = ' . $datestamp . '') as $row)
+        WHERE d.duty = \'Fitness Center\' AND s.shiftid = '. $shiftid .' AND su.date = '. $datestamp .'') as $row)
         {
           //fitness center
           array_push($FCempArray, $row['firstname']);
@@ -66,7 +66,7 @@ tr:nth-child(even) {
         JOIN submittedschedule su ON e.employeeid = su.employee
         JOIN duty d ON d.dutyid = su.duty
         JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'ICenter\' AND s.shiftid = '. $shiftid .' AND su.date = ' . $datestamp . '') as $row)
+        WHERE d.duty = \'ICenter\' AND s.shiftid = '. $shiftid .' AND su.date = '. $datestamp .'') as $row)
         {
           //fitness center
           array_push($ICempArray, $row['firstname']);
@@ -81,7 +81,7 @@ tr:nth-child(even) {
         JOIN submittedschedule su ON e.employeeid = su.employee
         JOIN duty d ON d.dutyid = su.duty
         JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'Equipment Room\' AND s.shiftid = '. $shiftid .' AND su.date = ' . $datestamp . '') as $row)
+        WHERE d.duty = \'Equipment Room\' AND s.shiftid = '. $shiftid .' AND su.date = '. $datestamp .'') as $row)
         {
           //fitness center
           array_push($ERempArray, $row['firstname']);
