90b9aca518213d18e84556858b59945699783d69
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 21:07:54 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -60,13 +60,11 @@ tr:nth-child(even) {
       function getICenterEmps($db, $shiftid, &$ICempArray, $datestamp)
       {
         $querystmt = $db->prepare("SELECT e.firstname, d.duty, s.shift FROM employee e
-        JOIN submittedschedule su ON e.employeeid = su.employee
-        JOIN duty d ON d.dutyid = su.duty
-        JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'ICenter\' AND s.shiftid = :shiftid AND su.date = :datestamp");
+        JOIN submittedschedule su ON e.employeeid = su.employee JOIN duty d ON d.dutyid = su.duty JOIN shift s ON s.shiftid = su.shift WHERE d.duty = \'ICenter\' AND s.shiftid = :shiftid AND su.date = :datestamp");
 
         $querystmt->bindValue(":datestamp", $datestamp, PDO::PARAM_STR);
         $querystmt->bindValue(":shiftid", $shiftid, PDO::PARAM_INT);
+
         $querystmt->execute();
         $rows = $querystmt->fetchAll(PDO::FETCH_ASSOC);
 
