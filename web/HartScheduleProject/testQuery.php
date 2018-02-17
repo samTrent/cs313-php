a67cf21d242f831b7150e720a62ee1df442a9aa7
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 21:16:53 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -59,21 +59,32 @@ tr:nth-child(even) {
 
       function getICenterEmps($db, $shiftid, &$ICempArray, $datestamp)
       {
-        echo '<p>preping query...</p>';
-        $querystmt = $db->prepare("SELECT e.firstname, d.duty, s.shift FROM employee e JOIN submittedschedule su ON e.employeeid = su.employee JOIN duty d ON d.dutyid = su.duty JOIN shift s ON s.shiftid = su.shift WHERE d.duty = \'ICenter\' AND s.shiftid = :shiftid AND su.date = :datestamp");
+        // echo '<p>preping query...</p>';
+        // $querystmt = $db->prepare("SELECT e.firstname, d.duty, s.shift FROM employee e JOIN submittedschedule su ON e.employeeid = su.employee JOIN duty d ON d.dutyid = su.duty JOIN shift s ON s.shiftid = su.shift WHERE d.duty = \'ICenter\' AND s.shiftid = :shiftid AND su.date = :datestamp");
+        //
+        // $querystmt->bindValue(":datestamp", $datestamp, PDO::PARAM_STR);
+        // $querystmt->bindValue(":shiftid", $shiftid, PDO::PARAM_INT);
+        //
+        // $querystmt->execute();
+        // $rows = $querystmt->fetchAll(PDO::FETCH_ASSOC);
+        // echo '<p>just before foreach</p>';
+        //
+        // foreach ($rows as $row)
+        // {
+        //     echo '<p>IN foreach</p>';
+        //   echo '<p>The row is ' . $row['firstname'] . '</p>';
+        //   array_push($ICempArray, $row['firstname']);
+        // }
 
-        $querystmt->bindValue(":datestamp", $datestamp, PDO::PARAM_STR);
-        $querystmt->bindValue(":shiftid", $shiftid, PDO::PARAM_INT);
-
-        $querystmt->execute();
-        $rows = $querystmt->fetchAll(PDO::FETCH_ASSOC);
-        echo '<p>just before foreach</p>';
-
-        foreach ($rows as $row)
+        foreach ($db->query('SELECT e.firstname, d.duty, s.shift FROM employee e
+        JOIN submittedschedule su ON e.employeeid = su.employee
+        JOIN duty d ON d.dutyid = su.duty
+        JOIN shift s ON s.shiftid = su.shift
+        WHERE d.duty = \'ICenter\' AND s.shiftid = '. $shiftid .' AND su.date = \''. $datestamp .'\'') as $row)
         {
-            echo '<p>IN foreach</p>';
-          echo '<p>The row is ' . $row['firstname'] . '</p>';
+          //fitness center
           array_push($ICempArray, $row['firstname']);
+
         }
 
       }
@@ -85,7 +96,7 @@ tr:nth-child(even) {
         JOIN submittedschedule su ON e.employeeid = su.employee
         JOIN duty d ON d.dutyid = su.duty
         JOIN shift s ON s.shiftid = su.shift
-        WHERE d.duty = \'Equipment Room\' AND s.shiftid = '. $shiftid .' AND su.date = \'02-17-2018\'') as $row)
+        WHERE d.duty = \'Equipment Room\' AND s.shiftid = '. $shiftid .' AND su.date = \''. $datestamp .'\'') as $row)
         {
           //fitness center
           array_push($ERempArray, $row['firstname']);
