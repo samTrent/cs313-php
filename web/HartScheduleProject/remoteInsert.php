384c8034cc41a89cdea9bd12c9c2f27dcec717a8
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 21:30:57 -0700

    next update

---
+++ web/HartScheduleProject/remoteInsert.php
@@ -0,0 +1,19 @@
+<?php
+try {
+  $host = "ec2-107-21-236-219.compute-1.amazonaws.com";
+  $dbname = "dcupbm4rvpsqb2";
+  $user = "zaqfmlhepcfhlm";
+  $password = "c9c64abec3d5de71334d351d883347b9e63e6a8b3a2d7fab5bbd3551f20112f4";
+  $port = "5432";
+
+  $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+
+
+  $stmt = $myDatabase ->prepare("INSERT INTO employee (firstname, lastname) VALUES (:firstname, :lastname)");
+
+  $stmt->bindValue(':firstname', $_POST['firstname'];, PDO::PARAM_STR);
+  $stmt->bindValue(':lastname', $_POST['lastname'];, PDO::PARAM_STR);
+
+  $stmt->execute();
+
+ ?>
