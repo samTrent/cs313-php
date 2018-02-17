69b5396691f5abb3dba56c633965c97dafecfd38
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 19:09:04 -0700

    next update

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -91,7 +91,7 @@ tr:nth-child(even) {
     //table headers
     echo '<tr>';
     echo '<th>Shift</th>';
-    foreach($db->query('SELECT date FROM submittedschedule WHERE submittedscheduleid = 1 AND submittedscheduleid = 2') as $row)
+    foreach($db->query('SELECT date FROM submittedschedule WHERE submittedscheduleid = 2') as $row)
     {
       //get dates
       echo '<th colspan="3">' . $row['date'] . '</th>';
