4c62229f0bd7ca116502b311492356defe865244
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-16 21:35:42 -0700

    next update

--- web/HartScheduleProject/remoteInsert.php
+++ web/HartScheduleProject/remoteInsert.php
@@ -20,3 +20,13 @@ try {
   $stmt->execute();
 
  ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>test</h1>
+   </body>
+ </html>
