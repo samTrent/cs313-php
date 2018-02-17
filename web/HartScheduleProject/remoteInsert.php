33625b2948f6605368aeaf887b6556d4da5d356d
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 11:23:15 -0700

    next update

--- web/HartScheduleProject/remoteInsert.php
+++ web/HartScheduleProject/remoteInsert.php
@@ -26,15 +26,17 @@ try {
   echo 'The date is: ' . $_POST['date'] . ' <br>';
   echo 'The shift is: ' . $_POST['shift'] . ' <br>';
 
-  $finalstmt = $db ->prepare("INSERT INTO submittedschedule (submitteddate, employee, shift, duty) VALUES (:submitteddate, :firstname, :shift, :duty)");
+  $finalstmt = $db ->prepare('INSERT INTO submittedschedule (submitteddate, employee, shift, duty) VALUES (:submitteddate, :firstname, :shift, :duty)');
 
-  $finalstmt->bindValue(':submitteddate', $_POST['date'], PDO::PARAM_STR);
-  $finalstmt->bindValue(':firstname', $employeeID, PDO::PARAM_INT);
-  $finalstmt->bindValue(':shift', $_POST['shift'], PDO::PARAM_INT);
-  $finalstmt->bindValue(':duty', $dutyID, PDO::PARAM_INT);
+  $finalstmt->bindValue(':submitteddate', $_POST['date']);
+  $finalstmt->bindValue(':firstname', $employeeID);
+  $finalstmt->bindValue(':shift', $_POST['shift']);
+  $finalstmt->bindValue(':duty', $dutyID);
   echo 'submitting schedule....<br>';
   $finalstmt->execute();
+
   echo 'SUCCESS!';
+
 
   // $stmt = $db ->prepare("INSERT INTO employee (firstname, lastname) VALUES (:firstname, :lastname)");
 
