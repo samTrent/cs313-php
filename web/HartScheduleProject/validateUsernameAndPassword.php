b15c526deaeadad6175b8faabcaa96b31783ecb1
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 16:27:13 -0700

    next update

--- web/HartScheduleProject/validateUsernameAndPassword.php
+++ web/HartScheduleProject/validateUsernameAndPassword.php
@@ -1,4 +1,12 @@
 <?php
+try {
+  $host = "ec2-107-21-236-219.compute-1.amazonaws.com";
+  $dbname = "dcupbm4rvpsqb2";
+  $user = "zaqfmlhepcfhlm";
+  $password = "c9c64abec3d5de71334d351d883347b9e63e6a8b3a2d7fab5bbd3551f20112f4";
+  $port = "5432";
+
+  $db = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
 
 
   $_POST['username'];
@@ -8,7 +16,7 @@
   $validPass = false;
 
   $stmt = $db ->prepare("SELECT username, password FROM users");
-  $stmt->bindValue(':firstname', $_POST['firstname'], PDO::PARAM_STR);
+
   $stmt->execute();
   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -18,8 +26,11 @@
     echo '<p> PASS: ' . $row['password'] . '</p>';
 
   }
-
-
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex </p>\n\n";
+ die();
+}
 
 
 
