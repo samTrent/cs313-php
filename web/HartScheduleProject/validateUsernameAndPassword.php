196c9a09fcc2dcb8c8ab88c999996a5f57814ef7
sd.trent@yahoo.com <sd.trent@yahoo.com>
2018-02-18 03:12:10 -0700

    finsing

--- web/HartScheduleProject/validateUsernameAndPassword.php
+++ web/HartScheduleProject/validateUsernameAndPassword.php
@@ -5,20 +5,19 @@ $_SESSION['loginError'];
 require("connectToDatabase.php");
 $db = getDatabaseConnection();
 
-  $_POST['username'];
-  $_POST['password'];
+  //sanitize the incoming post vars.
+  $incomingUsername = htmlspecialchars($_POST['username']);
+  $incomingPassword = htmlspecialchars($_POST['password']);
 
-  $validUser = false;
-  $validPass = false;
 
   //search database to see if the given username and password exsist.
   foreach ($db->query('SELECT username, password FROM users') as $row)
   {
     //check if user exsists
-    if($_POST['username'] == $row['username'])
+    if($incomingUsername == $row['username'])
     {
       //check the password
-      if($_POST['password'] == $row['password'])
+      if($incomingPassword == $row['password'])
       {
         $_SESSION['loginError'] = false; //there was no error
         $_SESSION['userIsLoggedIn'] = true;
