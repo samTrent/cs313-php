885dea661d083f3dc9b18d20b4e5a8dbec419c3d
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-17 17:15:57 -0700

    next update

--- web/HartScheduleProject/loginPage.php
+++ web/HartScheduleProject/loginPage.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="mycss.css">
     <meta charset="utf-8">
-    <title></title>
+    <title>Login</title>
   </head>
   <body>
 
+    <div class="center">
+
+
     <h1>Welcome to the Hart Schedule</h1>
     <h3>Please sign in</h3>
-
-
 
     <form class="" action="validateUsernameAndPassword.php" method="post">
       Username: <input type="text" name="username" value="" placeholder="Enter Username"><br>
       Password: <input type="password" name="password" value="" placeholder="Enter Password"><br>
       <input type="submit" name="" value="Login">
     </form>
+
+      </div>
 
   </body>
 </html>
