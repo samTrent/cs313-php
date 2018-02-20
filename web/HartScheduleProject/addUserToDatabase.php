2f9a3275282903adc69f63234d61c1cb5a5afd16
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-19 21:23:37 -0700

    next phase

--- web/HartScheduleProject/addUserToDatabase.php
+++ web/HartScheduleProject/addUserToDatabase.php
@@ -1,11 +1,23 @@
 <?php session_start();
-
-
+require('checkIfUserHasLoggedIn.php');
 require('connectToDatabase.php');
+
 $db = getDatabaseConnection();
 
-
-
-
-
  ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <p id="error"></p>
+   <body>
+     <form class="" onsubmit="return checkBothPasswordsMatch()" action="index.html" method="post">
+       Username: <input type="text" name="" value="" required>
+       Password: <input id="passwordFirst" type="text" name="" value="" required>
+       Confirm Password <input id="passwordSecond" type="text" name="" value="" required>
+     </form>
+   </body>
+ </html>
--- web/HartScheduleProject/engine.js
+++ web/HartScheduleProject/engine.js
@@ -10,3 +10,19 @@ function confirmDeleteTable()
 {
   return confirm("Are you sure you want to delete this table?");
 }
+
+//for new userpage
+function checkBothPasswordsMatch()
+{
+  if(document.getElementById("passwordFirst").value == document.getElementById("passwordSecond").value)
+  {
+    //submit form
+    return true;
+  }
+  else
+  {
+    //show error
+    document.getElementById('error').innerHTML = "Both passwords must match";
+    return false;
+  }
+}
