2ed7ff880efc78358da5deb5ec0e4ac7acbc4373
Sam Trent <samtrent@Sams-MacBook-Pro.local>
2018-02-19 21:25:52 -0700

    next phase

--- web/HartScheduleProject/addUserToDatabase.php
+++ web/HartScheduleProject/addUserToDatabase.php
@@ -16,9 +16,10 @@ $db = getDatabaseConnection();
    <p id="error"></p>
    <body>
      <form class="" onsubmit="return checkBothPasswordsMatch()" action="index.html" method="post">
-       Username: <input type="text" name="" value="" required>
-       Password: <input id="passwordFirst" type="text" name="" value="" required>
-       Confirm Password <input id="passwordSecond" type="text" name="" value="" required>
+       Username: <input type="text" name="" value="" required><br>
+       Password: <input id="passwordFirst" type="text" name="" value="" required><br>
+       Confirm Password <input id="passwordSecond" type="text" name="" value="" required><br>
+       <input type="submit" name="" value="Add User"><br>
      </form>
    </body>
  </html>
