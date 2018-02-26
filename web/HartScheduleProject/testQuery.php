0ec8a2e66ea898bb7535eb37546f36b74aa79983
sd.trent@yahoo.com <sd.trent@yahoo.com>
2018-02-25 21:03:22 -0700

    final

--- web/HartScheduleProject/testQuery.php
+++ web/HartScheduleProject/testQuery.php
@@ -23,7 +23,7 @@ require('getScheduleTables.php');
     <br><br>
     <!-- add user button -->
     <form style="float: right" class="" action="addUserToDatabase.php" method="post">
-      <input class="logoutButton" type="submit" name="" value="Create New User">
+      <input class="addUserButton" type="submit" name="" value="Create New User">
     </form>
 
     <!-- start php processing -->
